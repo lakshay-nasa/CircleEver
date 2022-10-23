8474e9d8ca8f48ab9cf6f6b7c7f8ca3848366f59
ADITI-2003 <aditidahiya1976@gmail.com>
2022-10-23 21:36:48 +0530

    Add SocityHelp files

---
+++ app/src/main/java/com/example/circleever/SocietyHelp1.kt
@@ -0,0 +1,4 @@
+package com.example.circleever
+
+class SocietyHelp1 {
+}
--- app/src/main/java/com/example/circleever/societyHome.kt
+++ app/src/main/java/com/example/circleever/societyHome.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.circleever.data.DataProvider
 import com.example.circleever.data.Society
